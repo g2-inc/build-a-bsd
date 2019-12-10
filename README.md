4f58fae443b3a77d9209c9f66afe448355b7536a
Shawn Webb <shawn.webb@hii-tsd.com>
2019-12-10 15:57:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Build A BSD
+
+License: 2-Clause BSD License
+Language: /bin/sh on HardenedBSD
+
+This collection of scripts provides automation for installing a
+customized, templated BSD installation onto a physical medium. This
+repo is meant to be ran from an mfsBSD-based solution, like SoloBSD.
+
+While support for multiple BSDs is planned, only HardenedBSD is
+supported at the moment.
+
+Boot from an mfsBSD, download this repo, untar it, and run
+`bin/bab.sh` as root.
---
+++ bin/bab.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#-
+# Copyright (c) 2019 Huntington Ingalls Industries
+# Author: Shawn Webb <shawn.webb@hii-tsd.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+get_topdir() {
+	local self
+
+	self=${1}
+
+	echo $(realpath $(dirname ${self}))
+	return ${?}
+}
+
+TOPDIR=$(realpath $(get_topdir ${0})/..)
+
+main() {
+	local res
+	local self
+	local verb
+
+	self=${1}
+	shift
+
+	while getopts 'v' o; do
+		case "${o}" in
+			v)
+				;;
+		esac
+	done
+
+	verb=${@[${OPTIND}]}
+
+	return ${res}
+}
+
+main ${0} $*
+exit ${?}
